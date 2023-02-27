9125e9c339f4c969b39394dd3a7d1314f51e7880
narendra28051991 <narendra28051991@gmail.com>
2023-02-27 11:45:11 +0100

    future age calculator complete

---
+++ javascript/javascript1/week1/js/age-ify.js
@@ -0,0 +1,6 @@
+alert("Let us start with\n\nAge-ify")
+console.log("Future Age Calculator!")
+const yearOfBirth = +prompt("Enter your birth year: ")
+const yearFuture = 2045
+const age = yearFuture - yearOfBirth
+console.log(`Your age will be ${age} years old in ${yearFuture}\n\n`)
