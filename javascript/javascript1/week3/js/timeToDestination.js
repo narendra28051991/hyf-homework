b55f0b4bf5d292479eab0c9a2e12d7f919e4b7f4
narendra28051991 <narendra28051991@gmail.com>
2023-03-13 11:16:53 +0100

    When will we be there?? (second method) complete

--- javascript/javascript1/week3/js/timeToDestination.js
+++ javascript/javascript1/week3/js/timeToDestination.js
@@ -5,6 +5,18 @@ const travelInformation = {
     destinationDistance: 432,
 };
 
+// First Method
+const timeToDestination = function (object) {
+    const maximumHoursToDestination = object.destinationDistance / object.speed;
+    const minimumHoursToDestination = Math.floor(maximumHoursToDestination);
+    const remainingHoursToDestination = maximumHoursToDestination - minimumHoursToDestination;
+    const minutesToDestination = Math.floor(remainingHoursToDestination * 60);
+    const timeToArrive = `${Math.floor(minimumHoursToDestination)} hours and ${minutesToDestination} minutes`;
+    return timeToArrive;
+}
+
+// Second Method
+/*
 const timeToDestination = function (object) {
     const timeToTravel = (object.destinationDistance / object.speed).toString();
     if (timeToTravel.includes('.')) {
@@ -19,6 +31,7 @@ const timeToDestination = function (object) {
         return `${timeToTravel} hours`;
     }
 }
+*/
 
 const travelTime = timeToDestination(travelInformation);
 console.log(travelTime);
