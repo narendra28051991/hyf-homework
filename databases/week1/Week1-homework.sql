466f3962d9cccceb965e81fdaf1f9cd111419e53
narendra28051991 <narendra28051991@gmail.com>
2023-06-03 10:33:28 +0200

    modified databases week-1 homework

--- databases/week1/Week1-homework.sql
+++ databases/week1/Week1-homework.sql
@@ -1,91 +1,84 @@
 /* Find out how many tasks are in the task table */
-select
-  COUNT(id)
-from
-  task;
+SELECT 
+    COUNT(id)
+FROM
+    task;
 
 /* Find out how many tasks in the task table do not have a valid due date */
-select
-  COUNT(id)
-from
-  task
-where
-  IsNull(due_date);
+SELECT 
+    COUNT(id)
+FROM
+    task
+WHERE
+    ISNULL(due_date);
 
 /* Find all the tasks that are marked as done */
-select
-  *
-from
-  task
-where
-  status_id = 3;
+SELECT 
+    task.id, title, due_date, status.name
+FROM
+    task
+        JOIN
+    status ON task.status_id = status.id
+WHERE
+    status.name = 'Done';
 
 /* Find all the tasks that are not marked as done */
-select
-  *
-from
-  task
-where
-  status_id != 3;
+SELECT 
+    task.id, title, due_date, status.name
+FROM
+    task
+        JOIN
+    status ON task.status_id = status.id
+WHERE
+    status.name != 'Done';
 
 /* Get all the tasks, sorted with the most recently created first */
-select
-  *
-from
-  task
-ORDER BY
-  created DESC;
+SELECT 
+    *
+FROM
+    task
+ORDER BY created DESC;
 
 /* Get the single most recently created task */
-select
-  *
-from
-  task
-ORDER BY
-  created DESC
+SELECT 
+    *
+FROM
+    task
+ORDER BY created DESC
 LIMIT 1;
 
 /* Get the title and due date of all tasks where the title or description contains database */
-select
-  *
-from
-  task
-where
-  title like '%database%'
-  OR
-  description LIKE '%database%';
+SELECT 
+    *
+FROM
+    task
+WHERE
+    title LIKE '%database%'
+        OR description LIKE '%database%';
 
 /* Get the title and status (as text) of all tasks */
-select
-  title,
-  case
-    WHEN status_id = 1 THEN 'Not started'
-    WHEN status_id = 2 THEN 'In progress'
-    WHEN status_id = 3 THEN 'Done'
-    ELSE ''
-  END AS status
-from
-  task;
+SELECT 
+    task.title, status.name
+FROM
+    task
+        INNER JOIN
+    status ON task.status_id = status.id;
 
 /* Get the name of each status, along with a count of how many tasks have that status */
-select
-  name,
-  COUNT(status_id) as NumberOfTasks
-from
-  status
-  INNER JOIN task ON status.id = task.status_id
-GROUP BY
-  status_id;
+SELECT 
+    name, COUNT(status_id) AS NumberOfTasks
+FROM
+    status
+        INNER JOIN
+    task ON status.id = task.status_id
+GROUP BY status_id;
 
 /* Get the names of all statuses, sorted by the status with most tasks first */
-select
-  name,
-  COUNT(status_id) as NumberOfTasks
-from
-  status
-  INNER JOIN task ON status.id = task.status_id
-GROUP BY
-  status_id
-ORDER BY
-  COUNT(status_id) DESC;
-  
+SELECT 
+    name, COUNT(status_id) AS NumberOfTasks
+FROM
+    status
+        INNER JOIN
+    task ON status.id = task.status_id
+GROUP BY status_id
+ORDER BY COUNT(status_id) DESC;
