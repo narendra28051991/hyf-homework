62923e728b501edc0a0e9d8206d72fc67793e12f
narendra28051991 <narendra28051991@gmail.com>
2023-06-10 08:37:27 +0200

    NodeJS week-1 pull request nodejs/week1/avg.js modified

--- nodejs/week1/avg.js
+++ nodejs/week1/avg.js
@@ -1,9 +1,10 @@
 const numbers = process.argv.slice(2);
 
 if (numbers.length > 0) {
-    const sum = numbers.reduce((a, b) => +a + +b);
-
-    numbers && isNaN(sum) ? console.log('Input should be a number') : console.log( sum / numbers.length );        
+    const isNotNumber = numbers.find(each => isNaN(+each) === true);
+    isNotNumber
+            ? console.log('Input should be a number')
+            : console.log( numbers.reduce((a, b) => +a + +b) / numbers.length );
 }
 else {
     console.log('Enter numbers to get the average')
