a3ff7fa0b5cbc9909c9b5ae7e605b48c22db55bf
narendra28051991 <narendra28051991@gmail.com>
2023-06-17 09:03:59 +0200

    moified app.js to include next and No Page found

--- nodejs/week2/app.js
+++ nodejs/week2/app.js
@@ -6,6 +6,10 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
@@ -16,3 +20,7 @@ app.get('/search', dbController.search_get);
 app.get('/documents', (req, res) => res.redirect('/'))
 app.get('/documents/:id', dbController.document_each);
 app.post('/search', dbController.search_post);
+
+app.use((req, res) => {
+    res.status(404).send('OOPS! No Page Found');
+});
